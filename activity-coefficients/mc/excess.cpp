1c0d35bcab5b0cd4f12819b6f7d968730b560361
Mikael Lund <mikael@Mikaels-MacBook-Pro.local>
2016-08-18 15:08:18 +0200

    Update

--- activity-coefficients/mc/excess.cpp
+++ activity-coefficients/mc/excess.cpp
@@ -11,13 +11,14 @@ int main() {
   Tspace spc(mcp);                              // simulation space
   Energy::Nonbonded<Tspace,Tpairpot> pot(mcp);  // hamiltonian
   pot.setSpace(spc);                            // share space w. hamiltonian
+  FormatXTC xtc(1000);
+
+  int xtc_freq = mcp["xtc_freq"] | 1000;        // frequency to save as XTC
 
   spc.load("state",Tspace::RESIZE);             // load old config. from disk (if any)
 
   // Two different Widom analysis methods
   double lB = pot.pairpot.first.bjerrumLength();// get bjerrum length
-  Analysis::Widom<PointParticle> widom1;        // widom analysis (I)
-  widom1.add(spc.p);
 
   Move::Propagator<Tspace> mv(mcp,pot,spc);
 
@@ -29,8 +30,14 @@ int main() {
   MCLoop loop(mcp);                             // class for handling mc loops
   while ( loop[0] ) {
     while ( loop[1] ) {
+
       sys+=mv.move();                           // move!
-      //widom1.sample(spc,pot,1);
+
+      if ( loop.innerCount() % xtc_freq == 0 ) {
+        xtc.setbox( spc.geo.len );
+        xtc.save( "traj.xtc", spc.p );
+      }
+
     }                                           // end of micro loop
     sys.checkDrift(Energy::systemEnergy(spc,pot,spc.p)); // calc. energy drift
     cout << loop.timing();
